8a36f1c20bf76113a4294e64153ef59ca20ff7f9
unknown <stefimartinez1298@gmail.com>
2024-05-14 19:30:31 -0300

    scrip de guarnis

---
+++ Scripts/guarniciones.js
@@ -0,0 +1,48 @@
+const recetas = [
+    {
+        nombre: "Ensalada Rusa",
+        imagen: "../images/Guarniciones/cebollitas-francesas-glaseadas.webp",
+        tiempoPreparacion: "30 minutos"
+    },
+    {
+        nombre: "Papas Fritas",
+        imagen: "..images/Guarniciones/champinones-salteados-con-jamon-y-ajo.webp",
+        tiempoPreparacion: "45 minutos"
+    },
+    // mas recetas
+];
+document.addEventListener("DOMContentLoaded", function() {
+    const recipesContainer = document.getElementById("recipes-container");
+
+    recetas.forEach(receta => {
+        // crea un div para la tarjeta de receta
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // crea img para la imagen de la receta
+        const imagen = document.createElement("img");
+        imagen.src = receta.imagen;
+        imagen.alt = "Imagen de la receta";
+
+        // Crea un div para los datos de la receta
+        const detailsRecipe = document.createElement("div");
+        detailsRecipe.classList.add("details-recipe");
+
+        // Crea un h2 para el nombre de la receta
+        const nombreReceta = document.createElement("h2");
+        nombreReceta.textContent = receta.nombre;
+
+        // Crea un p para el tiempo de preparación
+        const tiempoPreparacion = document.createElement("p");
+        tiempoPreparacion.textContent = "Tiempo de preparación: " + receta.tiempoPreparacion;
+
+        // Agrega la imagen, el nombre y el tiempo de preparación a la tarjeta de receta
+        card.appendChild(imagen);
+        detailsRecipe.appendChild(nombreReceta);
+        detailsRecipe.appendChild(tiempoPreparacion);
+        card.appendChild(detailsRecipe);
+
+        // Agrega la tarjeta de receta al contenedor de recetas
+        recipesContainer.appendChild(card);
+    });
+});
